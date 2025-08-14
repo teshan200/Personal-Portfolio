e5587bdc3224b370f56cddd10ae070847d1eae4b
teshan200 <git.teshan@gmail.com>
2025-08-15 00:24:19 +0530

    final

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,21 @@ trigger.addEventListener('click', () => {
     menu.classList.toggle('show');
     btn.classList.toggle('show');
 });
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let subject = document.getElementById("subject").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if (!name || !email || !subject || !message) {
+        alert("Please fill all fields!");
+        return;
+    }
+
+    document.getElementById("successMessage").style.display = "block";
+
+    document.getElementById("contactForm").reset();
+});
